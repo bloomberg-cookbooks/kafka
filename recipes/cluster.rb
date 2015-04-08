a8f3ed5b3e1f3e107bb8d0ca2015f7f451494c28
John Bellone <jbellone@bloomberg.net>
2015-04-08 11:53:28 -0400

    Add bits for zookeeper configuration in the cluster.

--- recipes/cluster.rb
+++ recipes/cluster.rb
@@ -4,6 +4,12 @@
 #
 # Copyright (C) 2015 Bloomberg Finance L.P.
 #
+
+# Zookeeper requires a unique, monotonic increasing identifier for all
+# members in the cluster. Kafka requires the same.
+node.default['zookeeper-cluster']['config']['server_id'] = KafkaCluster::Config.broker_id
+include_recipe 'zookeeper-cluster::quorum'
+
 kafka_cluster_config KafkaCluster::Config.cluster_name do
 
 end
