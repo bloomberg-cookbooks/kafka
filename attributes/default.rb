a5e787d93496f93ae62a0b4b360f58dd25bad18c
John Bellone <jbellone@bloomberg.net>
2015-04-08 11:42:56 -0400

    Add checksum and remote url attributes from dist.

--- attributes/default.rb
+++ attributes/default.rb
@@ -9,6 +9,6 @@ default['kafka-cluster']['cluster_name'] = 'kafka'
 default['kafka-cluster']['username'] = 'kafka'
 
 default['kafka-cluster']['init_type'] = :sysvinit
-default['kafka-cluster']['version'] = ''
-default['kafka-cluster']['remote_checksum'] = ''
-default['kafka-cluster']['remote_url'] = "http://mirror.cc.columbia.edu/pub/software/apache/kafka/kafka-%{version}/kafka-%{version}.tar.gz"
+default['kafka-cluster']['version'] = '0.8.2.1'
+default['kafka-cluster']['remote_checksum'] = 'a043655be6f3b6ec3f7eea25cc6525fd582da825972d3589b24912af71493a21'
+default['kafka-cluster']['remote_url'] = "http://mirror.cc.columbia.edu/pub/software/apache/kafka/kafka-%{version}/kafka-%{version}-src.tgz"
