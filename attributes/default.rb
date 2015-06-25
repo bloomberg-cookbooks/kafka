0cd8f496cfe65acbd550bfc8b2077733be889831
John Bellone <jbellone@bloomberg.net>
2015-06-25 14:16:16 -0400

    Updates the mirror to something we need.

--- attributes/default.rb
+++ attributes/default.rb
@@ -14,4 +14,4 @@ default['kafka-cluster']['config']['broker_id'] = 1
 default['kafka-cluster']['service']['version'] = '0.8.2.1'
 default['kafka-cluster']['service']['environment']['KAFKA_HEAP_OPTS'] = '-Xmx1G -Xms1G'
 default['kafka-cluster']['service']['binary_checksum'] = '89ede9ae0f51f7163c4140d8ab43fcedf8eb3066bb8058f3d97f75e9868899ce'
-default['kafka-cluster']['service']['binary_url'] = "http://apache.cs.utah.edu/kafka/%{version}/kafka_2.10-%{version}.tgz"
+default['kafka-cluster']['service']['binary_url'] = "http://mirror.cc.columbia.edu/pub/software/apache/kafka/%{version}/kafka_2.10-%{version}.tgz"
