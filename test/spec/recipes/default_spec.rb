eef5c62d410727bdc6f191f8a464a4e7e85bf163
John Bellone <jbellone@bloomberg.net>
2015-08-27 20:41:36 -0400

    Disables SELinux for all platforms.

--- test/spec/recipes/default_spec.rb
+++ test/spec/recipes/default_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe_recipe 'kafka-cluster::default' do
   cached(:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
 
-  it { expect(chef_run).to include_recipe('selinux::permissive') }
+  it { expect(chef_run).to include_recipe('selinux::disabled') }
   it { expect(chef_run).to include_recipe('java::default') }
   it { expect(chef_run).to include_recipe('sysctl::apply') }
   it { expect(chef_run).to create_poise_service_user('kafka').with(group: 'kafka') }
