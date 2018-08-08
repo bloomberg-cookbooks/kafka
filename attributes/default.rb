524304ba41463e744c5fc697c63a74f82f660437
Gennady Feldman <gfeldman18@bloomberg.net>
2018-08-07 22:10:37 -0400

    Bump Kafka version to 0.10.2.1
    * The old Kafka version is not available on mirrors anymore.
    * Also this version supports the same scala_version we have as
      default.

--- attributes/default.rb
+++ attributes/default.rb
@@ -38,10 +38,10 @@ default['kafka-cluster']['config']['log4j']['level']['kafka.log.LogCleaner'] = '
 default['kafka-cluster']['config']['log4j']['level']['state.change.logger'] = 'TRACE'
 
 default['kafka-cluster']['service']['mirror'] = 'http://mirror.cc.columbia.edu/pub/software/apache/kafka'
-default['kafka-cluster']['service']['version'] = '0.8.2.1'
+default['kafka-cluster']['service']['version'] = '0.10.2.1'
 default['kafka-cluster']['service']['scala_version'] = '2.12'
 default['kafka-cluster']['service']['environment']['KAFKA_HEAP_OPTS'] = '-Xmx1G -Xms1G'
-default['kafka-cluster']['service']['binary_checksum'] = '89ede9ae0f51f7163c4140d8ab43fcedf8eb3066bb8058f3d97f75e9868899ce'
+default['kafka-cluster']['service']['binary_checksum'] = 'e523b448b24a908202c35f12fd0caaa49ce70070305c360f7ed2270bb2c95f47'
 default['kafka-cluster']['service']['binary_url'] = "%{mirror}/%{version}/kafka_%{scala_version}-%{version}.tgz" # rubocop:disable Style/StringLiterals
 
 default['kafka-cluster']['ulimit']['filehandle_limit'] = 32_768
