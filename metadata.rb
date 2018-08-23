faea4492d8fef5400a05a6e55f4605bcfa4520f9
Gennady Feldman <gfeldman18@bloomberg.net>
2018-08-23 09:45:53 -0400

    Bumping cookbook version.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ issues_url 'https://github.com/bloomberg/kafka-cookbook/issues'
 source_url 'https://github.com/bloomberg/kafka-cookbook'
 description 'Application cookbook which installs and configures Apache Kafka.'
 long_description 'Application cookbook which installs and configures Apache Kafka.'
-version '1.3.10'
+version '1.4.0'
 chef_version '>= 12.1'
 
 supports 'ubuntu', '>= 12.04'
